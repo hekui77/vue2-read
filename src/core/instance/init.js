45e5c8b8f7c33f6b9c5839a7c91b2c37e9ff43a0
hekui <1287255589@qq.com>
2021-12-16 17:45:15 +0800

    'new_vue()'

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -13,9 +13,12 @@ import { extend, mergeOptions, formatComponentName } from '../util/index'
 let uid = 0
 
 export function initMixin (Vue: Class<Component>) {
+  // Vue 的初始化过程
   Vue.prototype._init = function (options?: Object) {
+    // Vue 实例
     const vm: Component = this
     // a uid
+    // 每个 Vue 实例都有一个 _uid ，并且是依次递增的
     vm._uid = uid++
 
     let startTag, endTag
@@ -29,12 +32,17 @@ export function initMixin (Vue: Class<Component>) {
     // 避免这种情况被观察到的标志
     vm._isVue = true
     // 合并选项
+    // 处理组件的配置项
     if (options && options._isComponent) {
       // 优化内部组件实例化
       // 因为动态选项合并非常慢，而且没有一个
       // 内部组件选项需要特殊处理
       initInternalComponent(vm, options)
     } else {
+      // 初始化组件走到这里，合并 Vue 的全局配置，比如 Vue.component 注册的全局组件会合并到根实例的 component 选项中
+      // 至于每个子组件的选项合并则发生在两个地方
+        // 1、Vue.component 方法注册的全局组件在注册时做了选项合并
+        // 2、{ component: { xxx } } 方法注册的局部组件在执行编译器生成的 render 函数时做了选项合并，包括根组件中的 component 配置
       vm.$options = mergeOptions(
         resolveConstructorOptions(vm.constructor),
         options || {},
@@ -42,6 +50,7 @@ export function initMixin (Vue: Class<Component>) {
       )
     }
     /* istanbul ignore else */
+    // 设置代理，将 vm 实例上的属性代理到 vm._renderProxy
     if (process.env.NODE_ENV !== 'production') {
       initProxy(vm)
     } else {
@@ -49,13 +58,22 @@ export function initMixin (Vue: Class<Component>) {
     }
     // expose real self
     vm._self = vm
+    // 初始化组件的实例关系属性，比如 $parent、$children、$root、$refs 等
     initLifecycle(vm)
+    // 初始化自定义事件，在这里需要注意，所以在<comp @click="handleClick" /> 上注册的事件，监听者不是父组件，
+    // 而是子组件本身，也就是说事件的派发和监听者都是子组件本身，和父组件无关
     initEvents(vm)
+    // 解析组件的插槽信息，得到 vm.$slot，处理渲染函数，得到 vm.$createElement 方法，即 h 函数
     initRender(vm)
+    // 调用 beforeCreate 钩子函数
     callHook(vm, 'beforeCreate')
+    // 初始化组件的 inject 配置项，得到 result[key] = val 形式的配置对象，然后对结果数据进行响应式处理，并代理到每个 key 到 vm 实例
     initInjections(vm) // resolve injections before data/props
+    // 数据响应式的重点，处理 props、methods、data、computed、watch
     initState(vm)
+    // 解析组件配置项上的 provide 对象，将其挂载到 vm._provided 属性上
     initProvide(vm) // resolve provide after data/props
+    // 调用 created 钩子函数
     callHook(vm, 'created')
 
     /* istanbul ignore if */
@@ -64,8 +82,10 @@ export function initMixin (Vue: Class<Component>) {
       mark(endTag)
       measure(`vue ${vm._name} init`, startTag, endTag)
     }
-
+    
+    // 如果发现配置项上有 el 选项，则自动调用 $mount 方法，也就是说有了 el 选项，就不需要再手动调用 $mount，反之，没有 el 则必须手动调用 $mount
     if (vm.$options.el) {
+      // 
       vm.$mount(vm.$options.el)
     }
   }
@@ -90,21 +110,28 @@ export function initInternalComponent (vm: Component, options: InternalComponent
   }
 }
 
+// 从组件构造函数中解析配置对象 options ，并合并基类选项
 export function resolveConstructorOptions (Ctor: Class<Component>) {
+  // 配置项目
   let options = Ctor.options
   if (Ctor.super) {
+    // 存在基类，递归解析基类构造函数的选项
     const superOptions = resolveConstructorOptions(Ctor.super)
     const cachedSuperOptions = Ctor.superOptions
     if (superOptions !== cachedSuperOptions) {
       // super option changed,
       // need to resolve new options.
+      // 说明基类构造函数选项已经发送冲突，需要重新设置
       Ctor.superOptions = superOptions
       // check if there are any late-modified/attached options (#4976)
+      // 检查 Ctor.options 上是否有任何后期修改/附加的选项
       const modifiedOptions = resolveModifiedOptions(Ctor)
       // update base extend options
+      // 如果存在被修改或增加的选项，则合并两个选项
       if (modifiedOptions) {
         extend(Ctor.extendOptions, modifiedOptions)
       }
+      // 选项合并，将合并结果赋值为 Ctor.options
       options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions)
       if (options.name) {
         options.components[options.name] = Ctor
@@ -114,10 +141,14 @@ export function resolveConstructorOptions (Ctor: Class<Component>) {
   return options
 }
 
+// 解析构造函数选项中后续被修改或者增加的选项
 function resolveModifiedOptions (Ctor: Class<Component>): ?Object {
   let modified
+  // 构造函数选项
   const latest = Ctor.options
+  // 密封的构造函数选项，备份
   const sealed = Ctor.sealedOptions
+  // 对比两个选项，记录不一致的选项
   for (const key in latest) {
     if (latest[key] !== sealed[key]) {
       if (!modified) modified = {}
